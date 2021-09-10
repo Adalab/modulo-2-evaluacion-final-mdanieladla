6619a701220095ede91be866f9510848125784d6
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-10 11:12:53 +0200

    add paintseries

--- src/js/00-search.js
+++ src/js/00-search.js
@@ -2,62 +2,53 @@
 //Array donde voy a guardar las series que me devuelve el servidor
 let series = [];
 //donde voy a pintar mis series
-const seriesList = document.querySelector('.js-series-list');
+let seriesList = document.querySelector('.js-series-list');
 //traemos el input del HTML
-const info = document.querySelector('.js-input');
-//cogemos el valor que escribe el usuario en el input
-const getInfo = info.value;
-//url + valor que mete el usuario ->`http://api.tvmaze.com/search/shows?q=${getInfo}`;
-const url = 'http://api.tvmaze.com/search/shows?q=girls';
+const input = document.querySelector('.js-input');
 //traemos el botón de HTML
 const btn = document.querySelector('.js-btn');
 
-
-function paintSeries() {
-  let html = '';
-  for (const serie of series) {
-   console.log(serie);
-   html += `<li class="js-series section__series--serie">`;
-   html += `<h3 class="name-serie">${serie.show}</h3>`;
-   html += `<img src="${serie.image}"`;
-   html += `</li>`;
-    }
-    seriesList.innerHTML = html;
+//funcion para que al pulsar el botón llame a la api
+function handleGetInfoSeries() {
+  getApi();
 }
 
-
-//NO FUNCIONA METER EL INPUT QUE QUIERAS Y QUE TE DEVUELVA LA SERIE
-//peticion al servidor
-function handleGetInfoSeries() {
+//funcion para llamar a la api
+function getApi() {
+  const getInfo = input.value;
+  const url = `http://api.tvmaze.com/search/shows?q=${getInfo}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
       series = data;
       paintSeries();
-    });
+});
 }
 
-btn.addEventListener('click', handleGetInfoSeries);
+//funcion para pintar las series
+function paintSeries() {
+  let html = '';
+  for (const serie of series) {
+    const serieTitle = serie.show.name;
+    let img = serie.show.image;
+    let imgDefault = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+    if (img === null) {
+      img = imgDefault;
+    } else {
+      img = serie.show.image.medium;
+    }
+    html += `<li class="list--li">`;
+    html += `<img src="${img}" width="350" height="350" class="serie-img"/>`;
+    html += `<h3 class="serie-title">${serieTitle}</h3>`;
+    html += `</li>`;
+  }
+  seriesList.innerHTML = html;
+}
 
-/*
-const btn = document.querySelector('.js-btn');
-
-const getInfoSerie = document.querySelector('.js-input');
-
-const getSerie = getInfoSerie.value;
-
-const url = '//api.tvmaze.com/search/shows?q=' + getSerie;
-
-
-function handleGetInfoSeries(ev) {
+//funcion para prevenir que el btn recargue la pagina por defecto
+function preventBtn(ev) {
   ev.preventDefault();
 }
 
 btn.addEventListener('click', handleGetInfoSeries);
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    series = data.name;
-});*/
+btn.addEventListener('click', preventBtn);
